f38c355f5a2a85a077536b73b1d85ccc7f28e689
rasyidahnuraini <rasyidahnuraini3156@gmail.com>
2026-05-25 08:38:08 +0700

    Menambahkan fitur read dan validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,29 +5,32 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
+    protected $userModel;
     function __construct()
     {
     helper('form');
+    $this->userModel = new UserModel();
     }
     public function login()
 {
     if ($this->request->getPost()) {
+        $rules = [
+        'username' => 'required|min_length[6]',
+        'password' => 'required|min_length[7]|numeric',
+        ];
+
+        if ($this->validate($rules)) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = [
-            'username' => 'rasyidah',
-            'password' => md5('123'),
-            'role' => 'admin',
-            'email' => '111202415721@mhs.dinus.ac.id'
-        ]; // passw 123
+        $dataUser = $this->userModel ->where(['username' => $username])->first();
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
-                date_default_timezone_set('Asia/Jakarta');
-
+        if ($dataUser) {
+	    if (password_verify($password, $dataUser['password'])) {
                 session()->set([
                 'username' => $dataUser['username'],
                 'role' => $dataUser['role'],
@@ -43,6 +46,10 @@ class AuthController extends BaseController
             }
         } else {
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+            return redirect()->back();
+        }
+        } else {
+            session()->setFlashdata('failed', $this->validator->listErrors());
             return redirect()->back();
         }
     } else {
